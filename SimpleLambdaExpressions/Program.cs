d96a07f0c265782f50db7bce9091fdc36ef19a3a
AlexandrTolstov <alextav_85@mail.ru>
2019-07-12 12:21:39 +0600

    Add project files.

---
+++ SimpleLambdaExpressions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Lambdas *****\n");
+            TraditionalDelegateSyntax();
+            Console.WriteLine($"{FuncLymbda(5, 10)}");
+
+        }
+        static int FuncLymbda(int a, int b) => a + b;
+        static void TraditionalDelegateSyntax()
+        {
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
+
+            Predicate<int> callback = IsEvenNumber;
+            List<int> eventNumbers = list.FindAll((i) => 
+            {
+                Console.WriteLine("value of i is currently: {0}", i);
+                bool isEven = ((i % 2) == 0);
+                return isEven;
+            });
+            Console.WriteLine("Here are your even numbers:");
+            foreach (int evenNumber in eventNumbers)
+            {
+                Console.WriteLine("{0}\t", evenNumber);
+            }
+            Console.WriteLine();
+        }
+        static bool IsEvenNumber(int i)
+        {
+            return (i % 2) == 0;
+        }
+    }
+}
